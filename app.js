3a49edcc46a7b1bfcd82c5948de5f63325064f7f
Shreyas <shreyas.kamath27@gmail.com>
2023-11-01 22:23:07 +0530

    COMMIT ADDED

--- app.js
+++ app.js
@@ -38,14 +38,6 @@ app.get("/",(req,res)=>{
     res.send("Nice working")
 })
 
-// app.get("/userid",async(req,res)=>{
 
-//     const {id} = req.body;
-//    const user = await User.findById(id)
-    
-//     res.json({
-//         success:true,
-//         user,
-//     })
 
 
